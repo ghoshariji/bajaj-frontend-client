1c62368689c26d72c313ecd82f4ed0c6aaf3559f
ghoshariji <144580430+ghoshariji@users.noreply.github.com>
2025-02-17 23:24:33 +0530

    das

--- src/component/layout.jsx
+++ src/component/layout.jsx
@@ -6,6 +6,7 @@ const Layout = ({ children }) => {
   return (
     <div className="flex h-screen">
       <Sidebar />
+      
       <div className="flex-1 flex flex-col">
         <Navbar />
         <main className="flex-grow p-1">{children}</main>
