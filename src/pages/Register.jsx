28dfdb7554c032698147bef9f25acf1760b29971
ghoshariji <144580430+ghoshariji@users.noreply.github.com>
2025-02-21 18:12:55 +0530

    dasda

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,6 +8,7 @@ const Register = () => {
     name: "",
     email: "",
     password: "",
+    phone:"",
     confirmPassword: "",
     termsAccepted: false,
   });
@@ -43,6 +44,7 @@ const Register = () => {
           name: formData.name,
           email: formData.email,
           password: formData.password,
+          phone:formData.phone
         }
       );
   
@@ -100,6 +102,24 @@ const Register = () => {
                   onChange={handleChange}
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="phone"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Your Phone no.
+                </label>
+                <input
+                  type="phone"
+                  name="phone"
+                  id="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="+91-xxxxxxx"
                   required
                 />
               </div>
