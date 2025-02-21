b83adf5aca50bbe12c463227e0a5b6006f2be02d
Niladri Sekhar Rout <niladri30012003@gmail.com>
2025-02-21 18:16:11 +0530

    push

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,6 +7,7 @@ const Register = () => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
+    phone:"",
     password: "",
     confirmPassword: "",
     termsAccepted: false,
@@ -103,6 +104,26 @@ const Register = () => {
                   required
                 />
               </div>
+
+
+              <div>
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Your email
+              </label>
+              <input
+                type="phone"
+                name="phone"
+                id="phone"
+                value={formData.phone}
+                onChange={handleChange}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="+123456789"
+                required
+              />
+            </div>
               <div>
                 <label
                   htmlFor="password"
